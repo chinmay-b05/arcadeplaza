5fa12318ada32e2b34485e78c5851127ee10d2c3
Chinmay Billava <141098334+chinmay-b05@users.noreply.github.com>
2023-11-18 18:57:48 +0530

    vf

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,17 +1,89 @@
 
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
- "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
-  <head>
-    <title>503 Backend unavailable, connection timeout</title>
-  </head>
-  <body>
-    <h1>Error 503 Backend unavailable, connection timeout</h1>
-    <p>Backend unavailable, connection timeout</p>
-    <h3>Error 54113</h3>
-    <p>Details: cache-del21734-DEL 1697858805 2388312115</p>
-    <hr>
-    <p>Varnish cache server</p>
-  </body>
-</html>
+let gameBoard = document.querySelector("#gameboard");
+let moreInfo = document.querySelector("#info");
+let endGame = document.querySelector(".endgame");
+let result = document.querySelector('.result');
+const blankCells = ["", "", "", "", "", "", "", "", ""];
+let turn = ['o', 'x']
+turn = turn[Math.floor(Math.random() * turn.length)];
+moreInfo.textContent =  `${turn} goes first`;
+
+function createBoard() {
+  blankCells.forEach((_cell, index) => {
+    const cellElement = document.createElement("div");
+    cellElement.classList.add("square");
+    cellElement.id = index;
+    cellElement.addEventListener("click", stamp);
+    gameBoard.append(cellElement);
+  });
+}
+
+createBoard();
+
+function stamp(e) {
+  const placeStamp = document.createElement("div");
+  placeStamp.classList.add(turn);
+  e.target.append(placeStamp);
+  turn = turn === "o" ? "x" : "o";
+  moreInfo.textContent = `It is now ${turn}'s turn`;
+  e.target.removeEventListener("click", stamp);
+
+  checkScore();
+}
+
+function checkScore() {
+  const allSquares = document.querySelectorAll(".square");
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  winningCombos.forEach((array) => {
+    const oHasWon = array.every((cell) =>
+      allSquares[cell].firstChild?.classList.contains("o")
+    );
+
+    if (oHasWon) {
+      endGame.classList.add("show-endgame");
+      result.textContent =  "O has won!!!"
+      moreInfo.textContent = "O has won!";
+      allSquares.forEach((square) =>
+        square.replaceWith(square.cloneNode(true))
+      );
+      return;
+    }
+  });
+
+  winningCombos.forEach((array) => {
+    const xHasWon = array.every((cell) =>
+      allSquares[cell].firstChild?.classList.contains("x")
+    );
+
+    if (xHasWon) {
+      endGame.classList.add("show-endgame");
+      result.textContent =  "X has won!!!"
+      moreInfo.textContent = "X has won!";
+      allSquares.forEach((square) =>
+        square.replaceWith(square.cloneNode(true))
+      );
+      return;
+    }
+  });
+}
+
+let restart = document.querySelector('.restart');
+
+restart.addEventListener('click', () => {
+  window.location.reload()
+})
+let exit = document.querySelector('.exit');
+
+exit.addEventListener('click', () => {
+  window.location.href = "tic-tac-toe.html";
+});
---
+++ would-you-rather/script.js
@@ -0,0 +1,19 @@
+async function fetchQuestions(){
+    let url = "https://would-you-rather-api.abaanshanid.repl.co/"
+    
+    let response = await fetch(url)
+    return await response.json()
+  }
+  
+  async function displayQuestions(){
+    let questions = await fetchQuestions()
+    let display = document.getElementById('question');
+    let num = document.getElementById('question-number');
+  
+    num.textContent = parseInt(questions.id) + 1;
+    display.textContent = questions.data;
+  }
+  
+  let btn = document.getElementById('button');
+  
+  btn.addEventListener('click', displayQuestions)
